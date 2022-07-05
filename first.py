c62b69c62db1bb5c9abb4aeabbba9e2825d3309b
czzt <2973085003@qq.com>
2022-07-05 16:55:18 +0800

    first

---
+++ first.py
@@ -0,0 +1,39 @@
+# print("hello word");
+# a=1;
+# b=2;
+# print(a+b);
+# #iyihxasbc
+# string="abandon"
+# string2="你好"
+# print(string[0:3]+string2)
+
+# def get_sum(a,b,c):
+#     q1=a
+#     q2=b
+#     return q1*b+c
+# print(get_sum(3,2,4))
+
+# abc[10]={0}
+# abc[0]=1
+
+
+# class Person:
+#     def _init_(self, name, hight, weight):
+#         self.name = name
+#         self.hight = hight
+#         self.weight = weight
+    
+#     def say_name(self):
+#         print("你好"+self.name)
+
+# #person1 = Person ("陈泽廷", 175, 45)
+
+# print(Person.weight)
+
+string = "hello World"
+print(string.find("llo"))
+print(string.capitalize())
+print(len(string))
+print(string.upper())
+print(string.lower())
+print(string.replace("hello","bad"))
