77592d023490d8f2e5513a8b4daf788e2a24408a
czzt <2973085003@qq.com>
2022-07-05 23:30:38 +0800

    daydaystudy

---
+++ hello.py
@@ -0,0 +1,10 @@
+# conding=utf-8
+
+#from turtle import Turtle
+
+
+x = '你好'
+y = True
+z = 20.0
+
+print(z)
---
+++ world.py
@@ -0,0 +1,10 @@
+# conding=utf-8
+
+
+from hello import x
+from hello import y as y2
+import hello
+
+print(x)
+print(y2)
+print(hello.z)
